46e00bee3b1b35857d69c1750b818ae09d2a818d
Jen Thomas <jenny_t152@yahoo.co.uk>
2019-07-06 15:15:24 +0100

    Added models for wildlife sites

--- WildlifeSites/wildlife_sites/admin.py
+++ WildlifeSites/wildlife_sites/admin.py
@@ -4,6 +4,12 @@ from django.contrib.gis.admin.options import OSMGeoAdmin
 import wildlife_sites.models
 
 # Register your models here.
+class TaxonomyAdmin(admin.ModelAdmin):
+    list_display = ('latin_name', 'class', 'order')
+    ordering = ('latin_name', 'class', 'order')
+    search_fields = ('latin_name', 'class', 'order')
+
+
 class SpeciesAdmin(admin.ModelAdmin):
     list_display = ('latin_name', 'common_name_english', 'common_name_catalan', 'common_name_spanish')
     ordering = ['common_name_english']
@@ -16,5 +22,62 @@ class SiteAdmin(OSMGeoAdmin):
     search_fields = ('name', 'location', 'country')
 
 
+class SourceTypeAdmin(admin.ModelAdmin):
+    list_display = ('type',)
+    ordering = ('type',)
+    search_fields = ('type',)
+
+
+class UriTypeAdmin(admin.ModelAdmin):
+    list_display = ('type',)
+    ordering = ('type',)
+    search_fields = ('type',)
+
+
+class UriAdmin(admin.ModelAdmin):
+    list_display = ('uri', 'uri_type')
+    ordering = ('uri', 'uri_type')
+    search_fields = ('uri', 'uri_type')
+
+
+class ReferenceTypeAdmin(admin.ModelAdmin):
+    list_display = ('type',)
+    ordering = ('type',)
+    search_fields = ('type',)
+
+
+class ReferenceAdmin(admin.ModelAdmin):
+    list_display = ('reference', 'reference_type')
+    ordering = ('reference', 'reference_type')
+    search_fields = ('reference', 'reference_type')
+
+
+class SourceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'source_type', 'uri', 'reference')
+    ordering = ('title', 'author', 'source_type', 'uri', 'reference')
+    search_fields = ('title', 'author', 'source_type', 'uri', 'reference')
+
+
+class TimeOfYearAdmin(admin.ModelAdmin):
+    list_display = ('month',)
+    ordering = ('month',)
+    search_fields = ('month',)
+
+
+class SiteVisitAdmin(admin.ModelAdmin):
+    list_display = ('site', 'species', 'months', 'source')
+    ordering = ('site', 'species', 'time_of_year', 'source')
+    search_fields = ('site', 'species', 'time_of_year', 'source')
+
+
+admin.site.register(wildlife_sites.models.Taxonomy, TaxonomyAdmin)
 admin.site.register(wildlife_sites.models.Species, SpeciesAdmin)
 admin.site.register(wildlife_sites.models.Site, SiteAdmin)
+admin.site.register(wildlife_sites.models.SourceType, SourceTypeAdmin)
+admin.site.register(wildlife_sites.models.UriType, UriTypeAdmin)
+admin.site.register(wildlife_sites.models.Uri, UriAdmin)
+admin.site.register(wildlife_sites.models.ReferenceType, ReferenceTypeAdmin)
+admin.site.register(wildlife_sites.models.Reference, ReferenceAdmin)
+admin.site.register(wildlife_sites.models.Source, SourceAdmin)
+admin.site.register(wildlife_sites.models.TimeOfYear, TimeOfYearAdmin)
+admin.site.register(wildlife_sites.models.SiteVisit, SiteVisitAdmin)
--- WildlifeSites/wildlife_sites/models.py
+++ WildlifeSites/wildlife_sites/models.py
@@ -1,22 +1,32 @@
 from django.contrib.gis.db import models
 
 # Create your models here.
+
+class Taxonomy(models.Model):
+    latin_name = models.CharField(max_length=255, unique=True)
+    taxclass = models.CharField(max_length=256, name='class', null=True, blank=True)
+    order = models.CharField(max_length=256, null=True, blank=True)
+
+    def __str__(self):
+        return "{}".format(self.latin_name)
+
+
 class Species(models.Model):
-    latin_name = models.CharField(max_length=1024)
-    common_name_english = models.CharField(max_length=1024)
-    common_name_catalan = models.CharField(max_length=1024)
-    common_name_spanish = models.CharField(max_length=1024)
+    latin_name = models.OneToOneField(Taxonomy, on_delete=models.PROTECT)
+    common_name_english = models.CharField(max_length=1024, null=True, blank=True)
+    common_name_catalan = models.CharField(max_length=1024, null=True, blank=True)
+    common_name_spanish = models.CharField(max_length=1024, null=True, blank=True)
 
     def __str__(self):
         return "{}".format(self.common_name_english)
 
 
 class Site(models.Model):
-    name = models.CharField(max_length=1024)
+    name = models.CharField(max_length=255, unique=True)
     position = models.PointField(null=True, blank=True)
     location = models.CharField(max_length=512)
     country = models.CharField(max_length=256)
-    notes = models.TextField(max_length=1024)
+    notes = models.TextField(max_length=1024, null=True, blank=True)
 
     def __str__(self):
         return "{}".format(self.name)
@@ -26,3 +36,68 @@ class Site(models.Model):
 
     def longitude(self):
         return self.location.x
+
+
+class SourceType(models.Model):
+    type = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return "{}".format(self.type)
+
+
+class UriType(models.Model):
+    type = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return "{}".format(self.type)
+
+
+class Uri(models.Model):
+    uri = models.CharField(max_length=1024)
+    uri_type = models.ForeignKey(UriType, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return "{}: {}".format(self.uri_type, self.uri)
+
+
+class ReferenceType(models.Model):
+    type = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return "{}".format(self.type)
+
+
+class Reference(models.Model):
+    reference = models.CharField(max_length=1024)
+    reference_type = models.ForeignKey(ReferenceType, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return "{}: {}".format(self.reference_type, self.reference)
+
+
+class Source(models.Model):
+    title = models.CharField(max_length=1024)
+    author = models.CharField(max_length=256, null=True, blank=True)
+    source_type = models.ForeignKey(SourceType, on_delete=models.PROTECT)
+    uri = models.OneToOneField(Uri, on_delete=models.PROTECT, null=True, blank=True)
+    reference = models.ForeignKey(Reference, on_delete=models.PROTECT, null=True, blank=True)
+
+    def __str__(self):
+        return "{}: {}".format(self.source_type, self.title)
+
+
+class TimeOfYear(models.Model):
+    month = models.IntegerField(unique=True)
+
+    def __str__(self):
+        return "{}".format(self.month)
+
+
+class SiteVisit(models.Model):
+    site = models.ForeignKey(Site, on_delete=models.PROTECT)
+    species = models.ForeignKey(Species, on_delete=models.PROTECT)
+    time_of_year = models.ManyToManyField(TimeOfYear, blank=True)
+    source = models.ForeignKey(Source, on_delete=models.PROTECT, null=True, blank=True)
+
+    def months(self):
+        return ",".join([str(t) for t in self.time_of_year.all()])
