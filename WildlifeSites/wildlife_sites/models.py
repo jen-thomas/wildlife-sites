55c9d1d1ad16a2fba43766fab4511966df806dd0
Jen Thomas <jenny_t152@yahoo.co.uk>
2020-03-16 21:27:16 +0000

    Changed length of taxonomy class

--- WildlifeSites/wildlife_sites/models.py
+++ WildlifeSites/wildlife_sites/models.py
@@ -4,7 +4,7 @@ from django.contrib.gis.db import models
 
 
 class TaxonomyClass(models.Model):
-    taxclass = models.CharField(max_length=256, unique=True)
+    taxclass = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return "{}".format(self.taxclass)
